1c065c6004f96cef55421c806f19d2fba634ac51
Gil Shotan <gilsho@cs.stanford.edu>
2021-05-20 03:21:11 +0300

    remove unncessary asset imports

--- src/core/components/Input.js
+++ src/core/components/Input.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import './styles/Input.css';
-import './assets/prompt.svg';
-import './assets/in.svg';
 
 
 // TODO import Autocomplete from './Autocomplete';
--- src/core/components/Line.js
+++ src/core/components/Line.js
@@ -3,9 +3,6 @@ import LineNav from './LineNav';
 import which from '../lib/which-type';
 import './styles/Line.css';
 import './styles/Types.css';
-import './assets/in.svg';
-import './assets/out.svg';
-import './assets/error.svg';
 
 class Line extends Component {
   constructor(props) {
